f03fa6ab04c80236c8e033676b0b1b8b3a2ea2a0
JhersonRojas <jhersonrojas.dev@gmail.com>
2023-12-20 18:54:29 -0500

    creando el proyecto base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-rick-and-morty');
+    expect(compiled.querySelector('h3')?.textContent).toContain('Angular with Rick and Morty');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
+// Angular features
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Components
+import { SearchComponent } from './components/search/search.component';
+import { ListComponent } from './components/list/list.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SearchComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'angular-rick-and-morty';
+  title = 'Angular with Rick and Morty';
 }
